77ae9b0bce872464a652d4472fee849a1b27525e
ChargeAhead <omertufan26@yandex.com>
2022-12-20 22:19:44 +0000

    substring

--- InfoTechJava2023/src/ders4/Substring.java
+++ InfoTechJava2023/src/ders4/Substring.java
@@ -4,7 +4,7 @@ public class Substring {
 
 	public static void main(String[] args) {
 		//substring ==> girdigimiz indexler arasindaki ifadeleri gormek icin kullaniriz.
-		
+		//             0123456789012345678901234567890123456
 		String str  = "Calisirsaniz, Java ogrenmek cok kolay";
 		System.out.println(str.substring(14)); 
 		System.out.println(str.substring(14,20)); //14.indexten itibaren alir 20 Dahil degildir.
